594b748760937dc4f99f96464ff30712ba151000
dite-code <edycadas@gmail.com>
2021-06-29 00:16:15 +0700

    fix sendtogame

--- root/wa/game-chat-api.php
+++ root/wa/game-chat-api.php
@@ -1,30 +1,8 @@
 <?php
 include("packet_class.php");
-$servername = "localhost";
-$username = "root";
-$password = "camelia";
-$dbname = "pw";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-
-
-function nama($no){
-	$data = mysqli_fetch_assoc(mysqli_query($GLOBALS['conn'],"SELECT * FROM users inner join rank on users.id=rank.userid where users.mobilenumber='$no' order by rank.time desc"));
-	//echo $data['rolename'];
-	return $data['rolename'];
-}
 
 function chat($from, $content){
-	//$from = nama($from);
-	$data = mysqli_fetch_assoc(mysqli_query($GLOBALS['conn'],"SELECT * FROM users inner join rank on users.id=rank.userid where users.mobilenumber='$from' order by rank.time desc"));
-	//$from = $data['rolename'];
-	$txt = "^ffdd99".$from.": ^ffdd00".$content;
+	$txt = "^ffdd99".$from.": ^ffdd00".$content.$data['rolename'];
 	$ChatBroadCast = new WritePacket();
 	$ChatBroadCast -> WriteUByte(9); 	//0:Umum, 1:Shout, 2:Party, 3:Guild, 7:Trade, 9:Sistem
 	$ChatBroadCast -> WriteUByte(0); 				//Emotion
@@ -33,8 +11,6 @@ function chat($from, $content){
 	$ChatBroadCast -> WriteOctets(""); 				//Data
 	$ChatBroadCast -> Pack(0x78); 					//Opcode
 	$ChatBroadCast -> Send("localhost", 29300);
-	//echo $txt;
 }
 
-//echo nama('6285373887411');
 ?>
--- root/wa/server.js
+++ root/wa/server.js
@@ -20,15 +20,15 @@ var mysql = require('mysql');
 var readbytes = 0;
 try {
 	if (fs.existsSync('bataschat.txt')) {
-	readbytes = Number(fs.readFileSync('bataschat.txt').toString());
-}
-else {
-	//console.log(readbytes);
-	fs.writeFileSync('bataschat.txt', readbytes.toString());
-}
-
-} catch(err) {
-  console.error(err)
+		readbytes = Number(fs.readFileSync('bataschat.txt').toString());
+	}
+	else {
+		//console.log(readbytes);
+		fs.writeFileSync('bataschat.txt', readbytes.toString());
+	}
+	
+	} catch(err) {
+	console.error(err)
 }
 
 const
@@ -99,39 +99,57 @@ async function connectToWhatsApp () {
 }
 
 
-function sendtogame(from, content){
-	execPhp('/root/whatsapp/game-chat-api.php', function(error, php, outprint){
-		// outprint is now `One'.
-		php.chat(from, content, function(err, result, output, printed){
+function sendtogame(a, b){
+	const from = a;
+	const content = b;
+	
+	var sql = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "camelia",
+		database: "pw"
+	});
+	sql.connect(function(err) {
+		if (err) throw err;
+		sql.query("SELECT * FROM users inner join rank on users.id=rank.userid where users.mobilenumber="+from+" order by rank.time desc", function (err, result, fields) {
+			if (err) throw err;
 
+			execPhp('/root/wa/game-chat-api.php', function(error, php, outprint){
+				//console.log(result[0].rolename + content);
+				
+				// outprint is now `One'.
+				php.chat(result[0].rolename, content, function(err, result, output, printed){
+					
+				});
+			});
 		});
 	});
+	;
 }
 
-
 function sendtowa(data){
-	 const idchar = data[0].substring(data[0].indexOf('src=')+4).split(' ')[0];
-	 const chl = data[0].substring(data[0].indexOf("chl=")+4).split(' ')[0];
-	 const msg = new Buffer.from(data[0].substring(data[0].indexOf("msg=")+4), 'base64').toString();
-	 
-	 if (chl==1){ 
-	 var sql = mysql.createConnection({
-		 host: "localhost",
-		 user: "root",
-		 password: "camelia",
-		 database: "pw"
-	 });
- 	 sql.connect(function(err) {
-	 if (err) throw err;
-	 sql.query("SELECT * FROM rank where roleid='"+idchar+"'", function (err, result, fields) {
-		 if (err) throw err;
-			 console.log(data[0].length, 'ID: '+result[0].rolename, 'Channel: '+chl, 'Msg: '+msg )
-			 const msgsend = '*'+result[0].rolename+'*: '+msg 
-			 //conn.sendMessage('6281233113454-1616044872@g.us', msgsend, 'conversation')
-			 conn.sendMessage('6283196825920-1624888416@g.us', msgsend, 'conversation')
-		 });
-	 });	
-	 }
+	const idchar = data[0].substring(data[0].indexOf('src=')+4).split(' ')[0];
+	const chl = data[0].substring(data[0].indexOf("chl=")+4).split(' ')[0];
+	const msg = new Buffer.from(data[0].substring(data[0].indexOf("msg=")+4), 'base64').toString();
+	
+	if (chl==1){ 
+		var sql = mysql.createConnection({
+			host: "localhost",
+			user: "root",
+			password: "camelia",
+			database: "pw"
+		});
+		sql.connect(function(err) {
+			if (err) throw err;
+			sql.query("SELECT * FROM rank where roleid='"+idchar+"'", function (err, result, fields) {
+				if (err) throw err;
+				console.log(data[0].length, 'ID: '+result[0].rolename, 'Channel: '+chl, 'Msg: '+msg )
+				const msgsend = '*'+result[0].rolename+'*: '+msg 
+				//conn.sendMessage('6281233113454-1616044872@g.us', msgsend, 'conversation')
+				conn.sendMessage('6283196825920-1624888416@g.us', msgsend, 'conversation')
+			});
+		});	
+	}
 }
 
 
@@ -140,28 +158,28 @@ function readsome() {
     if(stats.size<readbytes+1) {
         //console.log('Hehe I am much faster than your writer..! I will sleep for a while, I deserve it!');
         setTimeout(readsome, 500);
-    }
+	}
     else {
         fs.read(file, new Buffer.alloc(bite_size), 0, bite_size, readbytes, processsome);
-    }
+	}
 }
 
 function processsome(err, bytecount, buff) {
     //console.log('Read', bytecount, 'and will process it now.');
-
-     // Here we will process our incoming data:
-	 // Do whatever you need. Just be careful about not using beyond the bytecount in buff.
-	 const data = buff.toString('utf-8', 0, bytecount).split('\n');
-	 //console.log(bytecount)
-	 sendtowa(data)
-
-    // So we continue reading from where we left:
-    readbytes+=data[0].length+1;
+	
+	// Here we will process our incoming data:
+	// Do whatever you need. Just be careful about not using beyond the bytecount in buff.
+	const data = buff.toString('utf-8', 0, bytecount).split('\n');
+	//console.log(bytecount)
+	sendtowa(data)
+	
+	// So we continue reading from where we left:
+	readbytes+=data[0].length+1;
 	//fs.writeFileSync("bataschat.txt",readbytes,{encoding:'utf8',flag:'w'})
 	fs.writeFile("bataschat.txt", readbytes.toString(), (err) => {if (err) console.log(err);});
-    //console.log(readbytes);
+	//console.log(readbytes);
 	//readbytes+=bytecount;
-    process.nextTick(readsome);
+	process.nextTick(readsome);
 }
 
 connectToWhatsApp ().catch (err => console.log("unexpected error: " + err) ) // catch any errors
